863d852e7ff45bd29589de166a478697ac8617a7
triinlitwintschik-boop <triin.litwintschik@gmail.com>
2026-04-04 13:40:38 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>SlowFlow töötab 🚀</h1>
+      <p>Kui sa seda näed, siis kõik on korras!</p>
+    </div>
+  );
+}
+
+export default App;
